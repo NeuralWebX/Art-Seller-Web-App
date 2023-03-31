3f1e1bec5ff8417a2ccfafd66ff331e733bed12a
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2023-03-31 16:46:02 +0600

    Shop Task In Progress

--- resources/views/backend/pages/shop/index.blade.php
+++ resources/views/backend/pages/shop/index.blade.php
@@ -9,12 +9,22 @@
         <div class="col-md-4" onclick="location.href='{{ route('backend.product.show',$item->id) }}'">
             <div class="card text-white">
                 <img class="card-img-top" src="{{ $item->product_image }}" alt="Title">
-                <div class="card-body mb-3">
+                <div class="card-body">
                     <h4 class="card-title">{{ $item->product_name }}</h4>
                     <p class="card-text">{{ number_format($item->product_price,2) }} BDT.</p>
                     <p class="text-primary">Artist: {{ $item->user->name }}</p>
                 </div>
-                <a href="{{ route('backend.onlinePay.exampleEasyCheckout',$item->id) }}" class="btn btn-primary">Purchase Now</a>
+                <div class="row">
+                    <div class="col-md-5">
+                        <a href="{{ route('backend.onlinePay.exampleEasyCheckout',$item->id) }}"
+                            class="btn btn-primary">Book Now</a>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5">
+                        <a href="{{ route('backend.onlinePay.exampleEasyCheckout',$item->id) }}"
+                            class="btn btn-primary">Purchase Now</a>
+                    </div>
+                </div>
             </div>
         </div>
         @endforeach
