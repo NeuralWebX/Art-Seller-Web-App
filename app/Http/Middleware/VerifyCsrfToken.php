2542f9d465d561a85266ba4026a4250246a7569e
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2023-04-01 14:22:35 +0600

    ssl fixed issue

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,7 +11,8 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
+
     protected $except = [
-        //
+        'online-pay/pay-via-ajax', 'online-pay/success', 'online-pay/cancel', 'online-pay/fail',
     ];
 }
