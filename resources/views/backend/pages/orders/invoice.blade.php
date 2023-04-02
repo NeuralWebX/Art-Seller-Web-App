31c021a70ee37d6f0b429075e8710fdedd406552
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2023-04-02 13:37:57 +0600

    order and order slip done

--- resources/views/backend/pages/orders/invoice.blade.php
+++ resources/views/backend/pages/orders/invoice.blade.php
@@ -1,224 +1,153 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <style>
-        .top_rw {
-            background-color: #f4f4f4;
-        }
-
-        .td_w {}
-
-        button {
-            padding: 5px 10px;
-            font-size: 14px;
-        }
-
-        .invoice-box {
-            max-width: 890px;
-            margin: auto;
-            padding: 10px;
-            border: 1px solid #eee;
-            box-shadow: 0 0 10px rgba(0, 0, 0, .15);
-            font-size: 14px;
-            line-height: 24px;
-            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-            color: #555;
-        }
-
-        .invoice-box table {
-            width: 100%;
-            line-height: inherit;
-            text-align: left;
-            border-bottom: solid 1px #ccc;
-        }
-
-        .invoice-box table td {
-            padding: 5px;
-            vertical-align: middle;
-        }
-
-        .invoice-box table tr td:nth-child(2) {
-            text-align: right;
-        }
-
-        .invoice-box table tr.top table td {
-            padding-bottom: 20px;
-        }
-
-        .invoice-box table tr.top table td.title {
-            font-size: 45px;
-            line-height: 45px;
-            color: #333;
-        }
-
-        .invoice-box table tr.information table td {
-            padding-bottom: 40px;
-        }
-
-        .invoice-box table tr.heading td {
-            background: #eee;
-            border-bottom: 1px solid #ddd;
-            font-weight: bold;
+    <title>POS Slip</title>
+    <style type="text/css">
+        body {
+            font-family: Arial, sans-serif;
             font-size: 12px;
         }
 
-        .invoice-box table tr.details td {
-            padding-bottom: 20px;
+        .container {
+            border: 1px solid #ccc;
+            padding: 10px;
+            max-width: 300px;
+            margin: 0 auto;
         }
 
-        .invoice-box table tr.item td {
-            border-bottom: 1px solid #eee;
-        }
-
-        .invoice-box table tr.item.last td {
-            border-bottom: none;
-        }
-
-        .invoice-box table tr.total td:nth-child(2) {
-            border-top: 2px solid #eee;
+        .title {
+            font-size: 16px;
             font-weight: bold;
+            text-align: center;
+            margin-bottom: 10px;
         }
 
-        @media only screen and (max-width: 600px) {
-            .invoice-box table tr.top table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-            }
-
-            .invoice-box table tr.information table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-            }
+        .row {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 5px;
         }
 
-        /** RTL **/
-        .rtl {
-            direction: rtl;
-            font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        .label {
+            font-weight: bold;
+            flex-basis: 70px;
         }
 
-        .rtl table {
+        .value {
+            flex-basis: 200px;
             text-align: right;
         }
 
-        .rtl table tr td:nth-child(2) {
-            text-align: left;
+        .table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-bottom: 10px;
+        }
+
+        .table th,
+        .table td {
+            border: 1px solid #ccc;
+            padding: 5px;
+            text-align: center;
+        }
+
+        .table th {
+            background-color: #eee;
+            font-weight: bold;
+        }
+
+        .footer {
+            font-size: 10px;
+            margin-top: 10px;
+            text-align: center;
+        }
+
+        .signature {
+            margin-top: 20px;
+            text-align: center;
         }
     </style>
 </head>
 
 <body>
-    <div class="invoice-box">
-        <table cellpadding="0" cellspacing="0">
-            <tr class="top_rw">
-                <td colspan="2">
-                    <h2 style="margin-bottom: 0px;"> Invoice/Bill of ArtSeller </h2>
-                    <span style=""> Number: {{$order->id}} Date: {{$order->created_at}} </span>
-                </td>
-            </tr>
-            <tr class="top">
-                <td colspan="3">
-                    <table>
-                        <tr>
-                            <td>
-                                <b> Sold By: ArtSeller </b> <br>
-                                Uttara, Dhaka-1230<br>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <tr class="information">
-                <td colspan="3">
-                    <table>
-                        <tr>
-                            <td colspan="2">
-                                <b> Shipping Address: {{$order->address}} </b> <br>
-                                <b>Payment Status :</b> {{$order->payment_status}} <br>
-                                <b>Payment Method :</b> {{$order->payment_method}} <br>
-                                <b>Transaction ID :</b> {{$order->tran_id}} <br>
-                            </td>
-                            <td> <b> Billing Address: {{$order->address}} </b><br>
-                                <b>Reciever's Name :</b> {{$order->name}} <br>
-
-                                <b>Reciever's Phone :</b> {{$order->phone}}<br>
-                                <b>Reciever's Email :</b> {{$order->email}} <br>
-
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <td colspan="3">
-                <table cellspacing="0px" cellpadding="2px">
-                    <tr class="heading">
-                        <td style="width:25%;">
-                            ITEM
-                        </td>
-                        <td style="width:10%; text-align:center;">
-                            QTY.
-                        </td>
-                        <td style="width:10%; text-align:right;">
-                            UNIT PRICE (BDT.)
-                        </td>
-                        <td style="width:15%; text-align:right;">
-                            SUB TOTAL
-                        </td>
-                    </tr>
-                    {{-- @foreach ($order->details as $data)
-                    <tr class="item">
-                        <td style="width:25%;">
-                            {{$data->item->product_name}}
-                        </td>
-                        <td style="width:10%; text-align:center;">
-                            {{$data->quantity}}
-                        </td>
-                        <td style="width:10%; text-align:right;">
-                            {{$data->unit_price}}
-                        </td>
-                        <td style="width:15%; text-align:right;">
-                            {{$data->subtotal}}
-                        </td>
-                    </tr>
-                    @endforeach --}}
-            </td>
+    <div class="container">
+        <div class="title">Invoice/Bill of ArtSeller</div>
+        <div class="row">
+            <div class="label">Number:</div>
+            <div class="value">{{ $order->order_number }}</div>
+        </div>
+        <div class="row">
+            <div class="label">Date:</div>
+            <div class="value">{{ $order->created_at->format('y-m-d') }}</div>
+        </div>
+        <div class="row">
+            <div class="label">Sold By:</div>
+            <div class="value">ArtSeller,Uttara, Dhaka-1230</div>
+        </div>
+        <div class="row">
+            <div class="label">Payment Status:</div>
+            <div class="value">Pending</div>
+        </div>
+        <div class="row">
+            <div class="label">Payment Method:</div>
+            <div class="value"></div>
+        </div>
+        <div class="row">
+            <div class="label">Transaction ID:</div>
+            <div class="value"></div>
+        </div>
+        <div class="row">
+            <div class="label">Address:</div>
+            <div class="value">{{ $order->address }}</div>
+        </div>
+        <div class="row">
+            <div class="label">Email:</div>
+            <div class="value">{{ $order->email }}</div>
+        </div>
+        <div class="row">
+            <div class="label">Phone:</div>
+            <div class="value">{{ $order->phone }}</div>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ITEM</th>
+                    <th>QTY.</th>
+                    <th>UNIT PRICE (BDT.)</th>
+                    <th>SUB TOTAL</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ $order->orderDetails[0]->product->product_number }}</td>
+                    <td>{{ $order->orderDetails[0]->product->product_name }}</td>
+                    <td>1</td>
+                    <td>{{ $order->orderDetails[0]->product->product_price }}</td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="3">Total Amount :</td>
+                    <td>194.30 Taka Only</td>
+                </tr>
+            </tfoot>
         </table>
-        <tr class="total">
-            <td colspan="3" align="right"> Total Amount : <b> {{$order->amount}} Taka Only </b> </td>
-        </tr>
-        <tr>
-            <td colspan="3">
-                <table cellspacing="0px" cellpadding="2px">
-                    <tr>
-                        <td width="50%">
-                            <b> Declaration: </b> <br>
-                            We declare that this invoice shows the actual price of the goods
-                            described above and that all particulars are true and correct. The
-                            goods sold are intended for end user consumption and not for resale.
-                        </td>
-                        <td>
-                            * This is a computer generated invoice and does not
-                            require a physical signature
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="50%">
-                        </td>
-                        <td>
-                            <b>ArtSeller Sales Team</b> <br>
-                            <b>ArtSeller.com</b>
-                            <br>
-                            <hr>
-                            <b> Authorized Signature </b>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        </table>
+        <div class="declaration">
+            We declare that this invoice shows the actual price of the goods described above and that all particulars
+            are true
+            and correct. The goods sold are intended for end user consumption and not for resale. * This is a computer
+            generated
+            invoice and does not require a physical signature
+        </div>
+        <div class="footer">
+            ArtSeller Sales Team<br>
+            ArtSeller.com
+        </div>
+        <div class="signature">
+            Authorized Signature
+        </div>
     </div>
 </body>
 
